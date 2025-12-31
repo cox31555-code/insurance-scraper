0cbbb1b2e729b883190a4baf99eb6fc5c06d89a5
cox31555-code <cox31555@gmail.com>
2025-12-31 09:42:20 +0000

    main

--- server.js
+++ server.js
@@ -118,4 +118,5 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`API running on http://localhost:${port}`));
+const host = process.env.HOST || "0.0.0.0";
+app.listen(port, host, () => console.log(`API running on http://${host}:${port}`));
